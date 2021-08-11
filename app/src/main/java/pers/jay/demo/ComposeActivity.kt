e982cf06db822feeb6c15025a2ecca11ca88d0b4
RookieJay <1475485144@qq.com>
2021-08-11 18:24:48 +0800

    Title(fix):Remove usage of compose to prevent compile errors before Android Studio Fox. Cause：N/A Solution：N/A Effect: N/A Reviewer: N/A 附加说明：N/A

--- app/src/main/java/pers/jay/demo/ComposeActivity.kt
+++ app/src/main/java/pers/jay/demo/ComposeActivity.kt
@@ -1,10 +1,7 @@
 package pers.jay.demo
 
 import android.os.Bundle
-import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 
 /**
  * Copyright (c) 2011, 北京视达科技有限责任公司 All rights reserved.
@@ -16,17 +13,6 @@ class ComposeActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            test()
-        }
-    }
-
-    @Preview
-    @Composable
-    fun test() {
-//        Column {
-//            Text(text = "Hello Compose")
-//        }
     }
 
 }
